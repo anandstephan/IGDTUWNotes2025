e3fcfab474cfe0796f98ed4bb48e44b102c33422
Anand <anandstephan98@gmail.com>
2025-06-28 11:04:08 +0530

    feat:start react

---
+++ day9/firstapp/src/App.jsx
@@ -0,0 +1,11 @@
+import Hero from './components/Hero'
+const App = () =>{
+return <div>
+    <h1>Hello Saumya</h1>
+    <Hero heroName="Black Widow" realName="Natashaa"/>
+    <Hero heroName="Iron Man" realName="Tony Stark"/>
+    <Hero heroName="Captain America" realName="steve Roger"/>
+</div>
+}
+
+export default App
---
+++ day9/firstapp/src/components/Hero.jsx
@@ -0,0 +1,8 @@
+const Hero = (props) =>{
+
+return  <div>
+        <h1>Hello, My name is {props.heroName} a.k.a {props.realName} </h1>
+        </div>
+}
+
+export default Hero
